e44bbb60c84de0bc15d41b6b559421a000d0def9
Pavel Lysenko <plysenko@Pavels-Macbook.local>
2021-09-03 10:16:20 +0600

    Fixed linter hints

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,41 +9,41 @@ var ErrInvalidString = errors.New("invalid string")
 
 const zeroCode int32 = 48
 
-func multiplyRune(b *strings.Builder, rune int32, count int32) {
+func multiplyRune(b *strings.Builder, code int32, count int32) {
 	var i int32
 	for i = 0; i < count; i++ {
-		b.WriteRune(rune)
+		b.WriteRune(code)
 	}
 }
 
-func isRuneInteger(rune int32) bool {
-	return rune >= zeroCode && rune < zeroCode+10
+func isRuneInteger(code int32) bool {
+	return code >= zeroCode && code < zeroCode+10
 }
 
-func runeToInt32(rune int32) int32 {
-	return rune - zeroCode
+func runeToInt32(code int32) int32 {
+	return code - zeroCode
 }
 
 func Unpack(str string) (string, error) {
 	builder := strings.Builder{}
 	var current int32 = -1
 
-	for _, rune := range str {
-		if isRuneInteger(rune) {
-			//if string starts by digit or two digits-neighbors were found then return error
+	for _, code := range str {
+		if isRuneInteger(code) {
+			// if string starts by digit or two digits-neighbors were found then return error
 			if current == -1 {
 				return "", ErrInvalidString
 			}
-			multiplyRune(&builder, current, runeToInt32(rune))
+			multiplyRune(&builder, current, runeToInt32(code))
 			current = -1
 		} else {
 			if current != -1 {
 				builder.WriteRune(current)
 			}
-			current = rune
+			current = code
 		}
 	}
-	//the last character if string isn't ended by digit
+	// the last character if string isn't ended by digit
 	if current != -1 {
 		builder.WriteRune(current)
 	}
