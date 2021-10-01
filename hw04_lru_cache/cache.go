9e34b2af145c0002af1768cf8622586ad6a2ba0d
Pavel Lysenko <plysenko@Pavels-Macbook.spb.thumbtack.lo>
2021-10-01 10:31:47 +0300

    [HW04] Initial cache version checked in

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,15 +9,42 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
 }
 
+func (lc *lruCache) Set(key Key, value interface{}) bool {
+	if lc.items[key] == nil {
+		lc.items[key] = lc.queue.PushFront(&cacheItem{key: key, value: value})
+		if lc.queue.Len() > lc.capacity {
+			back := lc.queue.Back()
+			lc.queue.Remove(back)
+			delete(lc.items, back.Value.(*cacheItem).key)
+		}
+		return false
+	} else {
+		lc.items[key].Value.(*cacheItem).value = value
+		lc.queue.MoveToFront(lc.items[key])
+		return true
+	}
+}
+
+func (lc *lruCache) Get(key Key) (interface{}, bool) {
+	if lc.items[key] != nil {
+		lc.queue.MoveToFront(lc.items[key])
+		return lc.items[key].Value.(*cacheItem).value, true
+	}
+	return nil, false
+}
+
+func (lc *lruCache) Clear() {
+	lc.queue = NewList()
+	lc.items = make(map[Key]*ListItem, lc.capacity)
+}
+
 type cacheItem struct {
-	key   string
+	key   Key
 	value interface{}
 }
 
