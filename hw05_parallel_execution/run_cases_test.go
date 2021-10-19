59f03e9761b7180912b4d30a622137d326e9790e
Pavel Lysenko <plysenko@Pavels-Macbook.thumbtack.lo>
2021-10-19 12:02:36 +0600

    [HW05] Rewritten tests using suite and added a couple of new tests

---
+++ hw05_parallel_execution/run_cases_test.go
@@ -0,0 +1,115 @@
+package hw05parallelexecution
+
+import (
+	"fmt"
+	"math/rand"
+	"sync/atomic"
+	"time"
+)
+
+func (suite *parallelExecutionTestSuite) NextCase(testName string) *testCase {
+	switch testName {
+	case "TestFirstMErrors":
+		return &testCase{
+			workers:    10,
+			maxErrors:  23,
+			tasksCount: 50,
+			generator: func(ts *parallelExecutionTestSuite) []Task {
+				tasks := make([]Task, 0, ts.tc.tasksCount)
+				for i := 0; i < ts.tc.tasksCount; i++ {
+					err := fmt.Errorf("error from task %d", i)
+					tasks = append(tasks, func() error {
+						time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+						atomic.AddInt32(&ts.rs.totalRuns, 1)
+						atomic.AddInt32(&ts.rs.errors, 1)
+						return err
+					})
+				}
+				return tasks
+			},
+		}
+	case "TestNoErrors":
+		return &testCase{
+			workers:    5,
+			maxErrors:  1,
+			tasksCount: 50,
+			generator: func(ts *parallelExecutionTestSuite) []Task {
+				tasks := make([]Task, 0, ts.tc.tasksCount)
+				for i := 0; i < ts.tc.tasksCount; i++ {
+					taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
+					ts.rs.pureRunningTime += taskSleep
+					tasks = append(tasks, func() error {
+						time.Sleep(taskSleep)
+						atomic.AddInt32(&ts.rs.totalRuns, 1)
+						return nil
+					})
+				}
+
+				return tasks
+			},
+		}
+	case "TestWithRealWorkAndWOErrors":
+		return &testCase{
+			workers:    5,
+			maxErrors:  1,
+			tasksCount: 50,
+			generator: func(ts *parallelExecutionTestSuite) []Task {
+				tasks := make([]Task, 0, ts.tc.tasksCount)
+				for i := 0; i < ts.tc.tasksCount; i++ {
+					tasks = append(tasks, func(j int64) Task {
+						return func() error {
+							start := time.Now()
+							var k, np1, n, nm1 int64
+							n, nm1 = 1, 1
+							for k = 0; k < j; k++ {
+								np1 = n + nm1
+								nm1, n = n, np1
+							}
+							atomic.AddInt64((*int64)(&ts.rs.pureRunningTime), int64(time.Since(start)))
+							atomic.AddInt32(&ts.rs.totalRuns, 1)
+							return nil
+						}
+					}(int64(i+10)*(100*rand.Int63n(25000))))
+				}
+
+				return tasks
+			},
+		}
+	case "TestWithRealWorkAndWithSomeErrors":
+		return &testCase{
+			workers:    5,
+			maxErrors:  15,
+			tasksCount: 60,
+			generator: func(ts *parallelExecutionTestSuite) []Task {
+				tasks := make([]Task, 0, ts.tc.tasksCount)
+				for i := 0; i < ts.tc.tasksCount; i++ {
+					err := fmt.Errorf("error from task %d", i)
+					tasks = append(tasks, func(j int64) Task {
+						i := i
+						return func() error {
+							start := time.Now()
+							var k, np1, n, nm1 int64
+							n, nm1 = 1, 1
+							for k = 0; k < j; k++ {
+								np1 = n + nm1
+								nm1, n = n, np1
+							}
+							atomic.AddInt64((*int64)(&ts.rs.pureRunningTime), int64(time.Since(start)))
+							atomic.AddInt32(&ts.rs.totalRuns, 1)
+							if i%2 == 0 {
+								atomic.AddInt32(&ts.rs.errors, 1)
+								return err
+							} else {
+								return nil
+							}
+						}
+					}(int64(i+10)*(100*rand.Int63n(25000))))
+				}
+
+				return tasks
+			},
+		}
+	default:
+		return nil
+	}
+}
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -2,69 +2,85 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"fmt"
-	"math/rand"
-	"sync/atomic"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+
 	"go.uber.org/goleak"
 )
+
+type runStats struct {
+	totalRuns       int32
+	pureRunningTime time.Duration
+	elapsedTime     time.Duration
+	errors          int32
+}
+
+type testCase struct {
+	title      string
+	workers    int
+	maxErrors  int
+	tasksCount int
+	generator  func(ts *parallelExecutionTestSuite) []Task
+}
+
+type parallelExecutionTestSuite struct {
+	suite.Suite
+	rs    *runStats
+	tc    *testCase
+	tasks []Task
+}
+
+func (suite *parallelExecutionTestSuite) SetupTest() {
+	suite.rs = &runStats{}
+}
+
+func (suite *parallelExecutionTestSuite) BeforeTest(_, testName string) {
+	suite.tc = suite.NextCase(testName)
+	suite.tasks = suite.tc.generator(suite)
+}
+
+func (suite *parallelExecutionTestSuite) TestFirstMErrors() {
+	err := Run(suite.tasks, suite.tc.workers, suite.tc.maxErrors)
+
+	suite.Truef(errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+	suite.LessOrEqual(int(suite.rs.errors), suite.tc.maxErrors+suite.tc.workers)
+	suite.LessOrEqual(suite.rs.totalRuns, int32(suite.tc.workers+int(suite.rs.errors)), "extra tasks were started")
+}
+
+func (suite *parallelExecutionTestSuite) TestNoErrors() {
+	start := time.Now()
+	err := Run(suite.tasks, suite.tc.workers, suite.tc.maxErrors)
+	suite.rs.elapsedTime = time.Since(start)
+
+	suite.NoError(err)
+	suite.Equal(suite.rs.totalRuns, int32(suite.tc.tasksCount), "not all tasks were completed")
+	suite.LessOrEqual(int64(suite.rs.elapsedTime), int64(suite.rs.pureRunningTime/2), "tasks were run sequentially?")
+}
+
+func (suite *parallelExecutionTestSuite) TestWithRealWorkAndWOErrors() {
+	start := time.Now()
+	err := Run(suite.tasks, suite.tc.workers, suite.tc.maxErrors)
+	suite.rs.elapsedTime = time.Since(start)
+
+	suite.NoError(err)
+	suite.Equal(suite.rs.totalRuns, int32(suite.tc.tasksCount), "not all tasks were completed")
+	suite.LessOrEqual(int64(suite.rs.elapsedTime), int64(suite.rs.pureRunningTime/2), "tasks were run sequentially?")
+}
+
+func (suite *parallelExecutionTestSuite) TestWithRealWorkAndWithSomeErrors() {
+	start := time.Now()
+	err := Run(suite.tasks, suite.tc.workers, suite.tc.maxErrors)
+	suite.rs.elapsedTime = time.Since(start)
+
+	suite.Truef(errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+	suite.LessOrEqual(int(suite.rs.errors), suite.tc.maxErrors+suite.tc.workers)
+	suite.LessOrEqual(int64(suite.rs.elapsedTime), int64(suite.rs.pureRunningTime/2), "tasks were run sequentially?")
+}
 
 func TestRun(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
-	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
-		tasksCount := 50
-		tasks := make([]Task, 0, tasksCount)
-
-		var runTasksCount int32
-
-		for i := 0; i < tasksCount; i++ {
-			err := fmt.Errorf("error from task %d", i)
-			tasks = append(tasks, func() error {
-				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
-				atomic.AddInt32(&runTasksCount, 1)
-				return err
-			})
-		}
-
-		workersCount := 10
-		maxErrorsCount := 23
-		err := Run(tasks, workersCount, maxErrorsCount)
-
-		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
-		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
-	})
-
-	t.Run("tasks without errors", func(t *testing.T) {
-		tasksCount := 50
-		tasks := make([]Task, 0, tasksCount)
-
-		var runTasksCount int32
-		var sumTime time.Duration
-
-		for i := 0; i < tasksCount; i++ {
-			taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
-			sumTime += taskSleep
-
-			tasks = append(tasks, func() error {
-				time.Sleep(taskSleep)
-				atomic.AddInt32(&runTasksCount, 1)
-				return nil
-			})
-		}
-
-		workersCount := 5
-		maxErrorsCount := 1
-
-		start := time.Now()
-		err := Run(tasks, workersCount, maxErrorsCount)
-		elapsedTime := time.Since(start)
-		require.NoError(t, err)
-
-		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
-		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
-	})
+	suite.Run(t, new(parallelExecutionTestSuite))
 }
