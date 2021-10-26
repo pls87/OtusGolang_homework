c79dc2ba30ca23f7b002ae4eb286e4e3726a5f95
Pavel Lysenko <plysenko@Pavels-Macbook.local>
2021-10-26 11:12:37 +0600

    [HW06] Implemented first green version

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,31 @@ type (
 
 type Stage func(in In) (out Out)
 
+func runStage(done In, in In, stage Stage) Out {
+	out := make(Bi)
+	stageOut := stage(in)
+
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case val, ok := <-stageOut:
+				if !ok {
+					return
+				}
+				out <- val
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+	for _, s := range stages {
+		out = runStage(done, out, s)
+	}
+	return out
 }
