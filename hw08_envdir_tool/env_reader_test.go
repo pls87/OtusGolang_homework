5fedc01f442689fd40f0d1543dbca261282dc21e
Pavel Lysenko <plysenko@Pavels-Macbook.local>
2021-11-24 19:14:06 +0600

    [Hw08] Added tests for read_dir

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,68 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type testCase struct {
+	name     string
+	path     string
+	expected Environment
+	err      error
+}
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	cases := []testCase{
+		{
+			name: "simple case based on env testdata",
+			path: "./testdata/env",
+			expected: Environment{
+				"BAR":   EnvValue{Value: "bar", NeedRemove: false},
+				"EMPTY": EnvValue{Value: "", NeedRemove: false},
+				"FOO":   EnvValue{Value: "   foo\nwith new line", NeedRemove: false},
+				"HELLO": EnvValue{Value: "\"hello\"", NeedRemove: false},
+				"UNSET": EnvValue{Value: "", NeedRemove: true},
+			},
+			err: nil,
+		}, {
+			name: "case with plus sign and spaces in the name of variable",
+			path: "./testdata/more_tests/test_1",
+			expected: Environment{
+				"SPACES_in    THE_NAME": EnvValue{Value: "hmmm...what will happen in such case?", NeedRemove: false},
+				"TRIVIAL":               EnvValue{Value: "Nothing interesting...\nAbsolutely", NeedRemove: false},
+			},
+			err: nil,
+		}, {
+			name: "case with empty line and zero file",
+			path: "./testdata/more_tests/test_2",
+			expected: Environment{
+				"EMPTY_FIRST_LINE": EnvValue{Value: "", NeedRemove: false},
+				"ZERO_FILE":        EnvValue{Value: "", NeedRemove: true},
+			},
+			err: nil,
+		}, {
+			name: "case with no permission",
+			path: "./testdata/more_tests/test_2",
+			expected: Environment{
+				"EMPTY_FIRST_LINE": EnvValue{Value: "", NeedRemove: false},
+				"ZERO_FILE":        EnvValue{Value: "", NeedRemove: true},
+			},
+			err: nil,
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			env, e := ReadDir(tc.path)
+			if tc.err != nil {
+				require.ErrorIs(t, e, tc.err, "actual error %q but expected %q", e, tc.err)
+			} else {
+				require.NoError(t, e, "No error expected, but got %q", e)
+				require.Equal(t, tc.expected, env)
+			}
+		})
+	}
 }
