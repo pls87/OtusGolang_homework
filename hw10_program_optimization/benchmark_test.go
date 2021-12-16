bc9791814e2dff2f88cbbd40a90aefc53026c254
Pavel Lysenko <plysenko@plysenko.local>
2021-12-16 21:33:38 +0600

    [HW10] Simplified code a bit

--- hw10_program_optimization/benchmark_test.go
+++ hw10_program_optimization/benchmark_test.go
@@ -15,57 +15,27 @@ func BenchmarkCaseFromFrozenTest(b *testing.B) {
 	b.ReportAllocs()
 	b.StopTimer()
 
-	for i := 0; i < b.N; i++ {
-		r, _ := zip.OpenReader("testdata/users.dat.zip")
-		data, _ := r.File[0].Open()
+	r, _ := zip.OpenReader("testdata/users.dat.zip")
+	data, _ := r.File[0].Open()
 
-		b.StartTimer()
-		GetDomainStat(data, "biz")
-		b.StopTimer()
+	b.StartTimer()
+	GetDomainStat(data, "biz")
+	b.StopTimer()
 
-		data.Close()
-	}
+	data.Close()
 }
 
 func BenchmarkRandomData(b *testing.B) {
+	b.ReportAllocs()
 	b.StopTimer()
-	lines := generateRandomBuffer(1000)
+	bts := bytes.NewBufferString(generateRandomBuffer(1000))
+	suffix := gofakeit.DomainSuffix()
 	b.StartTimer()
-	b.ReportAllocs()
+
 	for i := 0; i < b.N; i++ {
-		GetDomainStat(bytes.NewBufferString(lines), gofakeit.DomainSuffix())
+		GetDomainStat(bts, suffix)
 	}
 	b.StopTimer()
-}
-
-func BenchmarkExtractEmail(b *testing.B) {
-	b.ReportAllocs()
-	b.StopTimer()
-	for i := 0; i < b.N; i++ {
-		gofakeit.Seed(time.Now().UnixMicro())
-		info := gofakeit.Person()
-
-		if bytes, e := jsoniter.Marshal(*info); e == nil {
-			b.StartTimer()
-			extractEmail(bytes)
-			b.StopTimer()
-		} else {
-			b.FailNow()
-		}
-	}
-}
-
-func BenchmarkHandleEmail(b *testing.B) {
-	b.StopTimer()
-	stat := DomainStat{}
-	for i := 0; i < b.N; i++ {
-		gofakeit.Seed(time.Now().UnixMicro())
-		email := gofakeit.Email()
-
-		b.StartTimer()
-		handleEmail(email, "com", &stat)
-		b.StopTimer()
-	}
 }
 
 func generateRandomBuffer(limit int) string {
--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -7,44 +7,26 @@ import (
 )
 import jsoniter "github.com/json-iterator/go"
 
-type User struct {
-	ID       int
-	Name     string
-	Username string
-	Email    string
-	Phone    string
-	Password string
-	Address  string
-}
-
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 	result := make(DomainStat)
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
-		email := extractEmail(scanner.Bytes())
-		handleEmail(email, domain, &result)
+		email := strings.ToLower(jsoniter.Get(scanner.Bytes(), "Email").ToString())
+		if !strings.HasSuffix(email, "."+domain) {
+			continue
+		}
+
+		parts := strings.SplitN(email, "@", 2)
+		if len(parts) != 2 {
+			continue
+		}
+
+		result[parts[1]]++
 	}
 	if scanner.Err() != nil {
 		return nil, scanner.Err()
 	}
 	return result, nil
-}
-
-func extractEmail(bytes []byte) string {
-	return strings.ToLower(jsoniter.Get(bytes, "Email").ToString())
-}
-
-func handleEmail(email, domain string, stat *DomainStat) {
-	if !strings.HasSuffix(email, "."+domain) {
-		return
-	}
-
-	parts := strings.SplitN(email, "@", 2)
-	if len(parts) != 2 {
-		return
-	}
-
-	(*stat)[parts[1]]++
 }
