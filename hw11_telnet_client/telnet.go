17c1e174940c842adef265342aa3ebd4c701495e
Pavel Lysenko <plysenko@plysenko.local>
2021-12-21 12:31:15 +0600

    [HW11] The first green version

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +14,43 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+type MyTelnetClient struct {
+	address string
+	timeout time.Duration
+	conn    net.Conn
+	in      io.ReadCloser
+	out     io.Writer
+}
+
+func (m *MyTelnetClient) Connect() error {
+	conn, e := net.DialTimeout("tcp", m.address, m.timeout)
+	if e != nil {
+		return fmt.Errorf("failed to dial: %w", e)
+	}
+
+	m.conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (m *MyTelnetClient) Close() error {
+	return m.conn.Close()
+}
+
+func (m *MyTelnetClient) Send() error {
+	_, e := io.Copy(m.conn, m.in)
+	return e
+}
+
+func (m *MyTelnetClient) Receive() error {
+	_, e := io.Copy(m.out, m.conn)
+	return e
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &MyTelnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
