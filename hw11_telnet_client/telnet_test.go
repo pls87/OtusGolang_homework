6c434db4ef5cc511239e46d124af76555f7231a5
Pavel Lysenko <plysenko@plysenko.local>
2021-12-23 17:20:12 +0600

    [HW11] Added timeout test

--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"io"
 	"io/ioutil"
 	"net"
@@ -15,45 +16,77 @@ import (
 
 type caseParams struct {
 	timeout          time.Duration
-	address          string
+	address2Listen   string
+	address2Connect  string
 	messages2Send    []string
 	messages2Receive []string
+	waitTimeoutError bool
+}
+
+type caseStatus struct {
+	portOpened      bool
+	clientConnected bool
+	in              *bytes.Buffer
+	out             *bytes.Buffer
+	err             error
 }
 
 type telnetTestSuite struct {
 	suite.Suite
 	params   caseParams
+	status   caseStatus
 	listener net.Listener
-	in       *bytes.Buffer
-	out      *bytes.Buffer
-
-	client TelnetClient
+	client   TelnetClient
 }
 
 func (s *telnetTestSuite) TearDownTest() {
-	s.NoError(s.listener.Close())
-	s.NoError(s.client.Close())
+	if s.status.portOpened {
+		s.NoError(s.listener.Close())
+	}
+	if s.status.clientConnected {
+		s.NoError(s.client.Close())
+	}
 }
 
 func (s *telnetTestSuite) TestBasicCase() {
 	s.params = caseParams{
 		timeout:          10 * time.Second,
-		address:          "127.0.0.1:",
 		messages2Send:    []string{"hello"},
 		messages2Receive: []string{"world"},
 	}
 
+	s.initConnections()
 	s.RunTest()
 }
 
-func (s *telnetTestSuite) RunTest() {
-	s.initTest()
+func (s *telnetTestSuite) TestSeveralMessagesCase() {
+	s.params = caseParams{
+		timeout:          10 * time.Second,
+		messages2Send:    []string{"hello", "cruel", "world"},
+		messages2Receive: []string{"bye-bye", "calm", "heaven"},
+	}
 
+	s.initConnections()
+	s.RunTest()
+}
+
+func (s *telnetTestSuite) TestTimeoutCase() {
+	s.params = caseParams{
+		timeout:          3 * time.Second,
+		address2Connect:  "127.1.0.1:5768",
+		waitTimeoutError: true,
+	}
+
+	s.initConnections()
+}
+
+func (s *telnetTestSuite) RunTest() {
 	var wg sync.WaitGroup
 	wg.Add(2)
 
 	go func() {
 		defer wg.Done()
+
 		s.sendMessagesFromClient()
 
 		s.NoError(s.client.Send())
@@ -76,19 +109,62 @@ func (s *telnetTestSuite) RunTest() {
 	wg.Wait()
 }
 
-func (s *telnetTestSuite) initTest() {
-	s.in = &bytes.Buffer{}
-	s.out = &bytes.Buffer{}
+func (s *telnetTestSuite) connectCheckStatus() {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		s.status.err = s.client.Connect()
+		s.status.clientConnected = s.status.err == nil
+	}()
+
+	if s.params.waitTimeoutError {
+		s.Eventuallyf(func() bool {
+			return s.status.err == nil
+		}, s.params.timeout, s.params.timeout-2*time.Second, "")
+
+		s.Eventuallyf(func() bool {
+			return s.status.err != nil
+		}, 4*time.Second, time.Second, "")
+
+		var result net.Error
+		s.True(errors.As(s.status.err, &result))
+		s.True(result.Timeout())
+		return
+	}
+
+	wg.Wait()
+	s.True(s.status.clientConnected)
+}
+
+func (s *telnetTestSuite) initConnections() {
+	s.status = caseStatus{
+		in:  &bytes.Buffer{},
+		out: &bytes.Buffer{},
+	}
+
+	address2Listen := s.params.address2Listen
+	if address2Listen == "" {
+		address2Listen = "127.0.0.1:"
+	}
 
 	var err error
-	s.listener, err = net.Listen("tcp", s.params.address)
+	s.listener, err = net.Listen("tcp", address2Listen)
 	s.NoError(err)
-	s.client = NewTelnetClient(s.listener.Addr().String(), s.params.timeout, ioutil.NopCloser(s.in), s.out)
-	s.NoError(s.client.Connect())
+	s.status.portOpened = true
+
+	address2Connect := s.params.address2Connect
+	if address2Connect == "" {
+		address2Connect = s.listener.Addr().String()
+	}
+
+	s.client = NewTelnetClient(address2Connect, s.params.timeout, ioutil.NopCloser(s.status.in), s.status.out)
+	s.connectCheckStatus()
 }
 
 func (s *telnetTestSuite) sendMessagesFromClient() {
-	s.sendMessages(s.params.messages2Send, s.in)
+	s.sendMessages(s.params.messages2Send, s.status.in)
 }
 
 func (s *telnetTestSuite) sendMessagesToClient(conn net.Conn) {
@@ -106,7 +182,7 @@ func (s *telnetTestSuite) checkMessagesFromClient(conn net.Conn) {
 }
 
 func (s *telnetTestSuite) checkMessagesToClient() {
-	s.checkMessages(s.params.messages2Receive, s.out)
+	s.checkMessages(s.params.messages2Receive, s.status.out)
 }
 
 func (s *telnetTestSuite) checkMessages(expected []string, r io.Reader) {
